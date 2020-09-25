bfbd5086c8b10947ae2e0f5ec293b8a77511de6c
Manuel Friedli <manuel@fritteli.ch>
2020-09-26 00:11:42 +0200

    Bugfixes.

--- dev-util/drone/files/drone.sh
+++ dev-util/drone/files/drone.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # $DRONE_CONFIG_FILE contains path to the config file
 
-if [[ -z "${DRONE_CONFIG_FILE}" ]] ; then
-	echo "DRONE_CONFIG_FILE not set!"
+if [[ -z "${DOCKER_DRONE_CONFIG_FILE}" ]] ; then
+	echo "DOCKER_DRONE_CONFIG_FILE not set!"
 	exit 1
 fi
 
-. "${DRONE_CONFIG_FILE}"
+. "${DOCKER_DRONE_CONFIG_FILE}"
 
 docker_args=""
 
 for var in "${!DRONE_@}" ; do
-	docker_args="${docker_args} --env=${var}=${!var} "
+	docker_args="${docker_args} --env=${var}=${!var}"
 done
 
 for p in ${DOCKER_PUBLISH} ; do
@@ -20,7 +20,7 @@ done
 
 docker run \
   --volume=/var/lib/drone:/data \
-  ${docker_args}
+  ${docker_args} \
   --restart=always \
   --detach=true \
   --name=drone \
